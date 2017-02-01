c68a47a942645db74f02773262c76c51b286adcf
kongming <luzhanneng@linghit.com>
2017-02-01 17:36:55 +0800

    用maven

--- myapplication/src/main/java/tongson/myapplication/MainActivity.java
+++ myapplication/src/main/java/tongson/myapplication/MainActivity.java
@@ -1,13 +1,25 @@
 package tongson.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.maventext.MavenActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView go= (TextView) findViewById(R.id.go);
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, MavenActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
